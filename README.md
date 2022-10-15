a55c34c91ad809bed5196508afcd593e28ab16d5
cmd <cmd@yggdrasil.onion>
2022-10-15 15:11:54 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# nostr-clightning-connect
-Magically connect to your lightning node using Nostr.
+# nostr-cl-connect
+Magically connect to your node using Nostr.
+
+## How to Use
+```bash
+## Clone this repo into your plugin folder.
+cd ~/.lightning/plugins
+git clone <this repo url>
+
+## Enter the folder and install dependencies.
+cd nostr-cl-connect
+yarn install || npm install
+
+## Add these configurations to your lightning config file.
+plugin=~/.lightning/plugins/nostr-cl-connect
+relayUrl=<domain name your nostr relay, minus the wss://>
+secretKey=<secret key used to encrypt/tag your traffic>
+```
+
+## Connecting a Client
+Simply install / import the `nostr-emitter` library into your project, and setup a `NostrEmitter` using the same relay and secret key as configured in your plugin. For more information, see https://github.com/cmdruid/nostr-emitter
+
+## Event API
+```js
+/* Use this event to make any RPC call. You will receive 
+ * a 'response' event with a data object. 
+ */
+
+emitter.emit('call', [ 
+  methodName,  // The name of the RPC call (as a string).
+  methodArgs   // A string, array of strings, or null.
+])
+
+emitter.on('response', (
+  eventParams, // The response object from your node.
+  metaData     // Metadata about the nostr event.
+) => {
+  // Your function logic goes here.
+})
+
+/* Subscribe to any events from your lightning node. */
+emitter.on('eventName', (
+  eventParams, // The response object from your node.
+  metaData     // Metadata about the nostr event.
+) => {
+  // Your function logic goes here.
+})
+
+/* List of subscribed events. */
+const notifications = [
+  'channel_opened',
+  'channel_open_failed',
+  'channel_state_changed',
+  'connect',
+  'disconnect',
+  'invoice_payment',
+  'invoice_creation',
+  'warning',
+  'forward_event',
+  'sendpay_success',
+  'sendpay_failure',
+  'coin_movement',
+  'balance_snapshot',
+  'openchannel_peer_sigs',
+  'shutdown',
+]
+
+const hooks = [
+  'peer_connected',
+  'commitment_revocation',
+  'db_write',
+  'openchannel',
+  'openchannel2',
+  'htlc_accepted',
+  'custommsg',
+  'onion_message_blinded',
+  'onion_message_ourpath'
+]
+```
+
+## Contributions
+Feel free to contribute!
+
+## Thanks
+Special thanks to [@niftynei](https://github.com/niftynei) for helping to squash a silly bug.
--- plugin.js
+++ plugin.js
@@ -5,12 +5,11 @@ const NostrEmitter = require('nostr-emitter')
 const Plugin = require('clightningjs')
 
 const DEFAULT_RELAYURL = 'nostr-relay.wlvs.space'
-const DEFAULT_SECRET   = getRandomBase64()
+const DEFAULT_SECRET   = getRandomHex()
 
 const nostrGateway = new Plugin()
 
 nostrGateway.emitter = new NostrEmitter()
-nostrGateway.emitter.log = nostrGateway.log
 
 const notifications = [
   'channel_opened',
@@ -31,38 +30,29 @@ const notifications = [
 ]
 
 const hooks = [
+  'peer_connected',
   'commitment_revocation',
   'db_write',
+  'openchannel',
+  'openchannel2',
+  'htlc_accepted',
+  'custommsg',
+  'onion_message_blinded',
+  'onion_message_ourpath'
 ]
 
-function getRandomBase64(size=16) {
+function getRandomHex(size=16) {
   const bytes = crypto.getRandomValues(new Uint8Array(size))
-  return Buffer.from(bytes).toString('base64')
+  return Buffer.from(bytes).toString('hex')
 }
 
 function registerListeners() {
-
   nostrGateway.emitter.on('call', async data => {
-
-
     const [ method, params = [] ] = data
-
     nostrGateway.log(`Received ${method} event with params: ${JSON.stringify(params)}`)
-
     if (nostrGateway?.rpc?.call) {  
       const response = await nostrGateway.rpc.call(method, params)
       nostrGateway.emitter.emit('response', [method, response])
-    }
-  })
-
-  nostrGateway.emitter.on('getinfo', async () => {
-    if (nostrGateway?.rpc?.call) {
-      const response = await nostrGateway.rpc.call('getinfo')
-      nostrGateway.emitter.emit('nodeinfo', {
-        balance: response.fees_collected_msat,
-        blockct: response.blockheight,
-        chain: response.network
-      })
     }
   })
 }
@@ -71,7 +61,6 @@ for (let eventName of notifications) {
   nostrGateway.subscribe(eventName)
   nostrGateway.notifications[eventName].on(eventName, params => {
     nostrGateway.emitter.emit(eventName, params)
-    return true
   })
 }
 
@@ -83,22 +72,21 @@ for (let hookName of hooks) {
 }
 
 nostrGateway.onInit = async params => {
-  nostrGateway.log('test')
+  const relayUrl = nostrGateway.options['relayUrl'].value
+  const secretKey = nostrGateway.options['secretKey'].value
 
-  const relayUrl = DEFAULT_RELAYURL //nostrGateway.options['relayUrl'].value
-  const secret   = DEFAULT_SECRET //nostrGateway.options['secret'].value
-
-  const connectString = Buffer.from(`${relayUrl}:${secret}`).toString('base64url')
+  const connectString = Buffer.from(`${relayUrl}:${secretKey}`).toString('base64url')
   const message = `Paste this connection string into your web app:\n\n${connectString}\n`
 
+  nostrGateway.emitter.log = (...msg) => nostrGateway.log(...msg)
   registerListeners(nostrGateway)
 
-  nostrGateway.emitter.connect('wss://' + relayUrl, secret)
-
+  nostrGateway.emitter.connect('wss://' + relayUrl, secretKey)
   nostrGateway.log(message)
+  return {'result': message }
 }
 
 nostrGateway.addOption('relayUrl', DEFAULT_RELAYURL, 'The url of the realy to connect with.')
-nostrGateway.addOption('secret', DEFAULT_SECRET, 'The secret used to connect with e2e encryption.')
+nostrGateway.addOption('secretKey', DEFAULT_SECRET, 'The secret used to connect with e2e encryption.')
 
 nostrGateway.start()
